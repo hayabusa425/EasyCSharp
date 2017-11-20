ef452cbbe5c707f812ceda3c337a75ea42381640
Hayato Hishiya <hayabusa425@gmail.com>
2017-11-20 14:46:03 +0900

    add practice

---
+++ Practice2/practice2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+/*
+class practice2
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello.");
+        Console.WriteLine("Good bye.");
+    }
+}
+*/
+
+class practice2
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "Sample";
+
+        Label lb = new Label();
+        lb.Text = "See you again!";
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
---
+++ Practice3/practice3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+/*
+class practice3
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "Sample";
+        fm.Width = 300;
+        fm.Height = 200;
+
+        Label lb = new Label();
+        lb.Text = "Hello.";
+        lb.Top = 50;
+        lb.Left = 100;
+        lb.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
+*/
+
+class practice3
+{
+    public static void Main()
+    {
+        Form fm = new Form();
+        fm.Text = "Sample";
+        fm.Width = 300;
+        fm.Height = 200;
+
+        Label lb1 = new Label();
+        Label lb2 = new Label();
+        lb1.Text = "hello";
+        lb2.Text = "Good bye";
+        lb2.Left = 100;
+        lb1.Parent = fm;
+        lb2.Parent = fm;
+
+        Application.Run(fm);
+    }
+}
